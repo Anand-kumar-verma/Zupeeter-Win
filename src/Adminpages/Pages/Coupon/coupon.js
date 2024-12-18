139fd920fc4eb1734e9421bac142bc34f64bd02c
sushmasingh2000 <sushmasingh16166@gmail.com>
2024-12-18 10:51:06 +0530

    hgg

--- src/Adminpages/Pages/Coupon/coupon.js
+++ src/Adminpages/Pages/Coupon/coupon.js
@@ -1,4 +1,5 @@
-import { Add } from "@mui/icons-material";
+import { Add, CopyAllSharp, CopyAllTwoTone } from "@mui/icons-material";
+import copy from "clipboard-copy";
 import { Button, Switch, TablePagination } from "@mui/material";
 import moment from "moment";
 import React from "react";
@@ -12,6 +13,10 @@ import CustomTable from "../../Shared/CustomTable";
 
 const Coupon = () => {
 
+    const functionTOCopy = (value) => {
+        copy(value);
+        toast.success("Copied to clipboard!");
+    };
     const client = useQueryClient()
     const navigate = useNavigate();
 
@@ -21,21 +26,21 @@ const Coupon = () => {
         {
             refetchOnMount: false,
             refetchOnReconnect: true,
-            refetchOnWindowFocus:false
+            refetchOnWindowFocus: false
         }
     );
     const coupon_data = data?.data?.data || [];
 
-   const UpdateCoupon = async (id)=>{
-    try{
-        const res = await axiosInstance.get(API_URLS?.update_coupon_record+id)
-        toast(res?.data?.msg,{id:1})
-        client.refetchQueries("coupon");
+    const UpdateCoupon = async (id) => {
+        try {
+            const res = await axiosInstance.get(API_URLS?.update_coupon_record + id)
+            toast(res?.data?.msg, { id: 1 })
+            client.refetchQueries("coupon");
+        }
+        catch (e) {
+            console.log(e)
+        }
     }
-    catch(e){
-        console.log(e)
-    }    
- }
 
     const tablehead = [
         <span>S.No.</span>,
@@ -52,7 +57,12 @@ const Coupon = () => {
     const tablerow = coupon_data?.map((i, index) => {
         return [
             <span>{index + 1}</span>,
-            <span>{i?.coupon_code}</span>,
+            <span>{i?.coupon_code} <CopyAllTwoTone className="!text-green-800 !text-lg"
+                onClick={() => {
+                    functionTOCopy(
+                        `${i?.coupon_code}`
+                    );
+                }} /></span>,
             <span>{i?.coupon_amount}</span>,
             <span>{i?.coupon_status}</span>,
             <span>{i?.coupon_limit}</span>,
@@ -60,9 +70,9 @@ const Coupon = () => {
             <span>{moment(i?.created_at).format("DD-MM-YYYY HH:mm:ss")}</span>,
             <span>
                 <Switch
-                checked={i?.coupon_status==="Active" ? true :false}
+                    checked={i?.coupon_status === "Active" ? true : false}
                     className="!text-green-600"
-                    onChange={()=>UpdateCoupon(i?.id)}
+                    onChange={() => UpdateCoupon(i?.id)}
                 />
             </span>
         ];
@@ -70,22 +80,22 @@ const Coupon = () => {
 
     return (
         <div>
-           <div className="!flex !justify-between">
-           <Button
-             className="!my-5 !bg-white"
-                onClick={() => navigate('/addcouponuser')}
-                endIcon={<Add />}
-            >
-                 Coupon To User
-            </Button>
-           <Button
-             className="!my-5 !bg-white"
-                onClick={() => navigate('/addcoupon')}
-                endIcon={<Add />}
-            >
-                Add Coupon
-            </Button>
-           </div>
+            <div className="!flex !justify-between">
+                <Button
+                    className="!my-5 !bg-white"
+                    onClick={() => navigate('/addcouponuser')}
+                    endIcon={<Add />}
+                >
+                    Coupon To User
+                </Button>
+                <Button
+                    className="!my-5 !bg-white"
+                    onClick={() => navigate('/addcoupon')}
+                    endIcon={<Add />}
+                >
+                    Add Coupon
+                </Button>
+            </div>
             <CustomTable
                 tablehead={tablehead}
                 tablerow={tablerow}
